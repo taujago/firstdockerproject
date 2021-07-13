67c9144fa7d53d5965a1491a784b9ce7c1b3c9ae
taujago <taujago@gmail.com>
2021-07-14 01:01:29 +0800

    final configuration

--- slave-init/01-initslavedb.sh
+++ slave-init/01-initslavedb.sh
@@ -6,7 +6,6 @@ create table test (id int auto_increment primary key, nama varchar(200));
 insert into test (nama) values('Madafakaa');
 -- end of prepare databae demo 
 
--- CHANGE MASTER TO MASTER_HOST='mysqlmaster',master_user='replicauser',master_password='rahasia',master_log_file='mysql-bin.000003',master_log_pos=156;
--- flush privileges;
--- START REPLICA; 
+CHANGE MASTER TO MASTER_HOST='mysqlmaster',master_user='replicauser',master_password='rahasia',master_log_file='mysql-bin.000003',master_log_pos=156;
+START REPLICA; 
 @@
